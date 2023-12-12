0a95d407864d14fcfa12cc7339e1b26a6774ee58
Palpha01 <113171530+Palpha01@users.noreply.github.com>
2023-12-12 18:11:28 +0400

    Updating Chapter 2's Exercise 2 a.py

--- Python Assessments Chapter II/Exercise 2/Exercise 2 a.py
+++ Python Assessments Chapter II/Exercise 2/Exercise 2 a.py
@@ -12,9 +12,9 @@ main = Tk()
 main.title('Exercise 2 a')
 
 Button(main, text="A", bg='red', relief=GROOVE, bd=5).pack(side=TOP, expand=YES, fill=X)
-Button(main, text="B", bg='yellow').pack(side=BOTTOM, expand=2, anchor=SW)
-Button(main, text="C", bg='blue').pack(side=LEFT, expand=2, pady=3, anchor=SE)
-Button(main, text="D", bg='white', relief=FLAT).pack(side=RIGHT, expand=2, anchor=S)
+Button(main, text="B", bg='yellow', padx=50).pack(side=BOTTOM, expand=2, anchor=S)
+Button(main, text="C", bg='blue',  padx=50, relief=FLAT).pack(side=LEFT, expand=2, anchor=SE)
+Button(main, text="D", bg='white', relief=FLAT, padx=50).pack(side=RIGHT, expand=2, anchor=S)
 
 main.resizable(True,True)
 main.mainloop()
--- Python Assessments Chapter II/Exercise 2/Exercise 2 b.py
+++ Python Assessments Chapter II/Exercise 2/Exercise 2 b.py
@@ -2,9 +2,6 @@
 
 # B: Square Grid
 # With the pack layout manager, Create the following labels inside the frames. A and B inside the left frame. C and D inside the right frame
-# Using Pack() align A and C at the top and B and D at the bottom of the frames that contain them
-# Support resizing. Use the ‘expand’ and ‘fill’ attributes of the pack method to make the labels grow and expand into the available space.
-# Assign border value as 5 to the frames
 
 import tkinter as tk
 
