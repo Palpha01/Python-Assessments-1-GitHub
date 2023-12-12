8efa79f1a2cd99de53997ab970d22db8e4dcdc9d
Palpha01 <113171530+Palpha01@users.noreply.github.com>
2023-12-13 03:09:13 +0400

    Adding Exercise 4 with image

--- Python Assessments Chapter II/Exercise 4.py
+++ Python Assessments Chapter II/Exercise 4.py
@@ -8,4 +8,10 @@ from tkinter import *
 
 main = Tk()
 
+Banner = PhotoImage(file='Images/RAK Bathspa.jpg')
+main.iconphoto(True,Banner)
+
+Banner.pack()
+
+main.resizable(True,True)
 main.mainloop()
