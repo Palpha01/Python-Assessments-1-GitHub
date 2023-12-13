8c1d974b7464078ef08f67f652dd744037b195b9
Palpha01 <113171530+Palpha01@users.noreply.github.com>
2023-12-13 12:17:38 +0400

    Renaming folders and Exercise 4

--- Python Assessments Chapter II/Exercise 4.py
+++ Python Assessments Chapter II/Exercise 4.py
@@ -7,11 +7,7 @@ import tkinter
 from tkinter import *
 
 main = Tk()
-
-Banner = PhotoImage(file='Images/RAK Bathspa.jpg')
-main.iconphoto(True,Banner)
-
-Banner.pack()
+ 
 
 main.resizable(True,True)
 main.mainloop()
