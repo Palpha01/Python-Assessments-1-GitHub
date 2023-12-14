2e668ab8698f2e889e56ed203e642b152b1d5cb1
Palpha01 <113171530+Palpha01@users.noreply.github.com>
2023-12-14 09:28:10 +0400

    Errors of Chapter 3

---
+++ Chapter III/Exercise 1.py
@@ -0,0 +1,18 @@
+# Exercise 1: Greeting App
+
+# Develop a GUI to greet the user. The app should have two frames: InputFrame and DisplayFrame.
+
+import tkinter
+
+from tkinter import *
+
+from tkinter import ttk
+
+main = Tk()
+
+
+main.title('Exercise 1')
+main.geometry('500x500')
+
+
+main.mainloop()
---
+++ Chapter III/Exercise 3.py
@@ -0,0 +1,5 @@
+# Exercise 3: Area Function
+
+# Develop a GUI application using tkinter that allows users to calculate and display the areas of various shapes
+# including circles, squares, and rectangles
+
