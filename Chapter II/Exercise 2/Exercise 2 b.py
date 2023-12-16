cdd1f12db89ca75fab89078078074f00457c05e3
Palpha01 <113171530+Palpha01@users.noreply.github.com>
2023-12-16 22:28:58 +0400

    Update Exercise 2 b.py

--- Chapter II/Exercise 2/Exercise 2 b.py
+++ Chapter II/Exercise 2/Exercise 2 b.py
@@ -11,13 +11,15 @@ main = Tk()
 
 main.title('Exercise 2 b')
 
-Frame1 = Frame(main,borderwidth=5,relief="solid").pack(side=LEFT,expand=True,fill='both')
-Frame2 = Frame(main,borderwidth=5,relief="solid").pack(side=RIGHT,expand=True,fill='both')
+Frame1 = Frame(main,border=5,relief="solid").pack(side=LEFT,expand=FALSE,fill="both")
 
-A = Label(Frame1,text="A",bg="#22263d",fg="white").pack(side=TOP,expand=True,fill="both")
-B = Label(Frame1,text="B",bg="white",fg="#22263d").pack(side=BOTTOM,expand=True,fill="both")
-C = Label(Frame2,text="C",bg="#22263d",fg="white").pack(side=TOP,expand=True,fill="both")
-D = Label(Frame2,text="D",bg="white",fg="#22263d").pack(side=BOTTOM,expand=True,fill="both")
+A = Label(Frame1,text="A",bg="#22263d",fg="white").pack(side=TOP,expand=TRUE,fill="both")
+B = Label(Frame1,text="B",bg="white",fg="#22263d").pack(side=BOTTOM,expand=TRUE,fill="both")
+
+Frame2 = Frame(main,border=5,relief="solid").pack(side=RIGHT,expand=FALSE,fill="both")
+
+C = Label(Frame2,text="C",bg="white",fg="#22263d").pack(side=TOP,expand=TRUE,fill="both")
+D = Label(Frame2,text="D",bg="#22263d",fg="white").pack(side=BOTTOM,expand=TRUE,fill="both")
 
 main.resizable(True,True)
 main.mainloop()
