bac155b3b03e8328e7f9a09116dcaabf2e4e2787
Palpha01 <113171530+Palpha01@users.noreply.github.com>
2023-12-21 04:48:51 +0400

    IV's exercise 2 is weird

--- Chapter IV/Exercise 2.py
+++ Chapter IV/Exercise 2.py
@@ -6,5 +6,35 @@
 # Hello my name is Peter Parker
 # I love Python Programming
 # Love
-# Enemy  
+# Enemy
 
+import tkinter
+from tkinter import *
+
+def count_string_occurrences():
+    target_strings = [
+        "Hello my name is Peter Parker",
+        "I love Python Programming",
+        "Love",
+        "Enemy"
+    ]
+
+with open("Python-Assessments-1-GitHub/Chapter IV/Texts/Sentences.txt",'r') as file:
+    data = file.read()
+    
+occurrences = [data.count(string) for string in target_strings]
+
+result.config(text="Occurrences: {occurrences}")
+
+main = Tk()
+main.title("Exercise 2")
+
+instructions = Label(main,text="Count the string occurrences in the file")
+instructions.pack()
+
+count = Button(main,text="Count Occurrences",command=count_string_occurrences)
+count.pack()
+
+result = Label(main,text="Occurrences: ")
+
+main.mainloop()
